646c3c962d8b437eaa999381031a5ec46273783a
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-29 19:37:48 -0500

    re used the open struct instead guess user

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -8,9 +8,20 @@ module CurrentUserConcern
   end
 
   def guest_user
-    OpenStruct.new(name: 'Guest User',
-                   first_name: 'Guest',
-                   last_name: 'User',
-                   email: 'guest@email.com')
+    OpenStruct.new(
+      name: 'Guest User',
+      first_name: 'Guest',
+      last_name: 'User',
+      email: 'guest@email.com'
+    )
   end
+
+  # def guest_user
+  # guest = GuestUser.new
+  # guest.name = 'Guest User'
+  # guest.first_name = 'Guest'
+  # guest.last_name = 'User'
+  # guest.email = 'guest@example.com'
+  # guest
+  # end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,23 +11,24 @@ module ApplicationHelper
   end
 
   # def login_helper
-    # if current_user.is_a?(User)
-      # link_to 'Logout', destroy_user_session_path, method: :delete
-    # else
-      # (link_to 'Sign In', new_user_session_path) +
-        # '<br/>'.html_safe +
-        # (link_to 'Register', new_user_registration_path)
-      # end
+  # if current_user.is_a?(User)
+  # link_to 'Logout', destroy_user_session_path, method: :delete
+  # else
+  # (link_to 'Sign In', new_user_session_path) +
+  # '<br/>'.html_safe +
+  # (link_to 'Register', new_user_registration_path)
+  # end
   # end
 
   def login_helper
     if current_user.is_a?(User)
+    # if current_user.is_a?(GuestUser)
       link_to 'Logout', destroy_user_session_path, method: :delete
     else
       (link_to 'Sign In', new_user_session_path) +
         '<br/>'.html_safe +
         (link_to 'Register', new_user_registration_path)
-      end
+end
   end
 
   def source_helper(_layout_name)
