3d12820a79196717d017e8e8554f98ef8d38b356
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-30 02:34:52 -0500

    applied the style argument to the login_helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,32 +2,13 @@
 
 # Module for the ApplicatioHelper
 module ApplicationHelper
-  def sample_helper
-    out = ''
-    out << "<p>
-    My helper </p>"
-    # .html_safe
-    out.html_safe
-  end
-
-  # def login_helper
-  # if current_user.is_a?(User)
-  # link_to 'Logout', destroy_user_session_path, method: :delete
-  # else
-  # (link_to 'Sign In', new_user_session_path) +
-  # '<br/>'.html_safe +
-  # (link_to 'Register', new_user_registration_path)
-  # end
-  # end
-
-  def login_helper
-    # if current_user.is_a?(User)
+  def login_helper(style)
     if current_user.is_a?(GuestUser)
-      (link_to 'Sign In', new_user_session_path) +
-        '<br/>'.html_safe +
-        (link_to 'Register', new_user_registration_path)
+      (link_to 'Sign In', new_user_session_path, class: style) +
+        ' '.html_safe +
+        (link_to 'Register', new_user_registration_path, class: style)
     else
-      link_to 'Logout', destroy_user_session_path, method: :delete
+      link_to 'Logout', destroy_user_session_path, method: :delete, class: style
     end
   end
 
