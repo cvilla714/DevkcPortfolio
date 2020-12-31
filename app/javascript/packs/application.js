ac47bf93f59f8097b5de789fd8aa4fc12bcf7db9
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-31 18:54:52 -0500

    added fontawesome and also the icons for github, twitter, linked and their respective links

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,9 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
-import "../stylesheets/application" 
+import "../stylesheets/application"
+import "@fortawesome/fontawesome-free/js/all";
+
  
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
