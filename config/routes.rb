3aea752aaeff92fc6f60a6467c17257d74f5cd80
Cosmel Villalobos <cosmelvg@gmail.com>
2021-01-08 17:00:34 -0500

    added the routes for the user and topics

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users,path: '',path_names: { sign_up: 'register', sign_in: 'login', sign_out: 'logout'}
-  resources :portfolios,except: [:show]
-  get 'angular-items',to: 'portfolios#angular'
-  get 'rails-items',to: 'portfolios#rails'
-  get 'service-items',to: 'portfolios#service'
-  get 'portfolio/:id',to: 'portfolios#show', as: 'portfolio_show'
-  # get 'pages/home' 
+  devise_for :users, path: '', path_names: { sign_up: 'register', sign_in: 'login', sign_out: 'logout' }
+  resources :users
+  resources :portfolios, except: [:show]
+  get 'angular-items', to: 'portfolios#angular'
+  get 'rails-items', to: 'portfolios#rails'
+  get 'service-items', to: 'portfolios#service'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  # get 'pages/home'
   # get 'pages/about'
   # get 'pages/contact'
   # get 'about', to: 'pages#about'
@@ -15,11 +16,12 @@ Rails.application.routes.draw do
   # get 'contact-', to: 'pages#contact'
   get 'contact-me-to-work', to: 'pages#contact'
   resources :blogs do
+    resources :topics
     member do
-      get:toggle_status
+      get :toggle_status
     end
   end
 
-  root to: 'pages#home' #this will replace this code  # get 'pages/home' 
+  root to: 'pages#home' # this will replace this code  # get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
