38eb2e7e3de5ff6d445d0e0805e5cfd6a2ddba80
Cosmel Villalobos <cosmelvg@gmail.com>
2021-01-09 15:30:16 -0500

    added the method most_recent to sort the blogs in ascending order

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -9,4 +9,8 @@ class Blog < ApplicationRecord
   belongs_to :topic
   belongs_to :author, class_name: 'User'
   validates :title, :body, presence: true
+
+  def self.most_recent
+    Blog.order(created_at: :asc)
+  end
 end
