2d5e0a6b692162c4aa083cc2d6d55e2370732ed2
Cosmel Villalobos <cosmelvg@gmail.com>
2021-01-08 21:46:54 -0500

    eliminated the validation for the title not to the unique in order to be able to update any blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
   validates_presence_of :title, :body
   validates :title, length: { in: 4..25 }
-  validates :title, uniqueness: true
   belongs_to :topic
   belongs_to :author, class_name: 'User'
   validates :title, :body, presence: true
