827cc1ce3d8370ec4f2f316718dec8e26ee52308
Cosmel Villalobos <cosmelvg@gmail.com>
2021-01-09 15:31:16 -0500

    created the most recent blog using the method most_recent and extracting the first one

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,15 @@
+# frozen_string_literal: true
+
 class Topic < ApplicationRecord
   has_many :blogs
   validates :title, presence: true
+
+  def most_recent_blog
+    if blogs.most_recent.nil?
+      arr = []
+      arr.push(Blog.new)
+    else
+      blogs.most_recent.limit(1)
+    end
+  end
 end
