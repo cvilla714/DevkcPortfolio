a8bf672f2bb171ef3d9b2be37eda3b2f19559e26
Cosmel Villalobos <cosmelvg@gmail.com>
2021-01-12 15:17:56 -0500

    increased the number of characters for the title from 25 up to 100

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,7 +3,7 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   validates_presence_of :title, :body
-  validates :title, length: { in: 4..25 }
+  validates :title, length: { in: 4..100 }
   belongs_to :topic
   belongs_to :author, class_name: 'User'
   validates :title, :body, presence: true
