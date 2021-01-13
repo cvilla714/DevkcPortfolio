5d11529e7b66a83f9c91601eef9f6482f3306e5e
Cosmel Villalobos <cosmelvg@gmail.com>
2021-01-13 12:35:52 -0500

    created the method for ordering the porfolios in ascending order

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -10,6 +10,11 @@ class Portfolio < ApplicationRecord
   # where(subtitle: 'Angular')
   #   end
 
+
+  def self.by_position
+    order("position ASC")
+  end
+
   scope :angular, -> { where(subtitle: 'Angular') }
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
