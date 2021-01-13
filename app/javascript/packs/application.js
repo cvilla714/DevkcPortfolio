ae10f410e408697e37b05b2a927840ffc20eaec0
Cosmel Villalobos <cosmelvg@gmail.com>
2021-01-13 18:42:19 -0500

    tried to implement the background image but no success yet

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,6 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-require("jquery")
-require("jquery-ui")
-require("packs/html.sortable");
 
 import "bootstrap";
 import "@fortawesome/fontawesome-free/js/all";
