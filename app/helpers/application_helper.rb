0f0a019009585dc47bb92377f42a733718347622
Cosmel Villalobos <cosmelvg@gmail.com>
2021-01-13 00:35:04 -0500

    created the nav helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,5 +19,17 @@ module ApplicationHelper
     content_tag(:p, greetings, class: 'source-greetings')
     # end
   end
+
+  def nav_helper style, tag_type
+nav_links = <<NAV
+<#{tag_type}><a href="#{root_path}",class="#{style}">Home</a></#{tag_type}>
+<#{tag_type}><a href="#{about_me_path}",class="#{style}">About</a></#{tag_type}>
+<#{tag_type}><a href="#{contact_me_to_work_path}",class="#{style}">Contact</a></#{tag_type}>
+<#{tag_type}><a href="#{blogs_path}",class="#{style}">Blog</a></#{tag_type}>
+<#{tag_type}><a href="#{portfolios_path}",class="#{style}">Portfolio</a></#{tag_type}>
+NAV
+      nav_links.html_safe
+  end
+
 end
 # rubocop:enable Lint/UnderscorePrefixedVariableName
