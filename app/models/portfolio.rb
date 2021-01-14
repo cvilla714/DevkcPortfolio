74aa4009d3f698da5daa6f1ed4b4984b2e2ad2c3
Cosmel Villalobos <cosmelvg@gmail.com>
2021-01-14 15:46:35 -0500

    cretaed the concern for the image generator helper method

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   has_many :technologies
   validates :title, :body, :main_image, :thumb_image, presence: true
   validates :title, length: { in: 5..25 }
@@ -26,7 +27,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= 'https://via.placeholder.com/600x400'
-    self.thumb_image ||= 'https://via.placeholder.com/350x200'
+    self.main_image ||= Placeholder.image_genertor(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_genertor(height: '350', width: '200')
   end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,11 +1,12 @@
 class Skill < ApplicationRecord
+  include Placeholder
   validates :title, :percent_utilized, presence: true
   validates :title, length: { in: 4..25 }
 
-  # after_initialize :set_defaults
-  #
-  # def set_defaults
-  # self.bade ||= 'https://via.placeholder.com/400.png/09f/fff%20C/O%20https://placeholder.com/'
-  # self.thumb_image ||= 'https://via.placeholder.com/200.png/09f/fff%20C/O%20https://placeholder.com/'
-  # end
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.badge ||= Placeholder.image_genertor(height:'250',width:'250')
+    # self.badge ||= 'https://via.placeholder.com/250x250'
+  end
 end
