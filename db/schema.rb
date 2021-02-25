b8cc64f18f5cdb01b412a902d9888a6a48a22524
Cosmel Villalobos <cosmelvg@gmail.com>
2021-02-25 14:26:19 -0500

    new schema without the thumb_image column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_16_051639) do
+ActiveRecord::Schema.define(version: 2021_02_25_192354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,7 +75,6 @@ ActiveRecord::Schema.define(version: 2021_01_16_051639) do
     t.string "subtitle"
     t.text "body"
     t.text "main_image"
-    t.text "thumb_image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "position"
