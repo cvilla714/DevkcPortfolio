f869af7c0703c75f7c0e6ffe974b5ded185748ac
Cosmel Villalobos <cosmelvg@gmail.com>
2021-02-25 15:01:21 -0500

    dropped the skills table

---
+++ db/migrate/20210225195520_drop_skills.rb
@@ -0,0 +1,4 @@
+class DropSkills < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_192354) do
+ActiveRecord::Schema.define(version: 2021_02_25_195520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,14 +78,6 @@ ActiveRecord::Schema.define(version: 2021_02_25_192354) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "position"
-  end
-
-  create_table "skills", force: :cascade do |t|
-    t.string "title"
-    t.integer "percent_utilized"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.text "badge"
   end
 
   create_table "technologies", force: :cascade do |t|
